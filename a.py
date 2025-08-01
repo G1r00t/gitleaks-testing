5ef86bf03089fb074dd88ccb775746761b97908f
Gr00t <130218709+G1r00t@users.noreply.github.com>
2025-08-01 19:44:56 +0530

    testing

---
+++ a.py
@@ -0,0 +1,13 @@
+[core]
+    repositoryformatversion = 0
+    filemode = true
+    bare = false
+    logallrefupdates = true
+
+[remote "origin"]
+    url = https://gho_dxqAER6A0qhzkNbfebpD210EMFSBly23x5z8@github.com/CD-Internal-Projects/User-Insight-Tracking-UI.git
+    fetch = +refs/heads/*:refs/remotes/origin/*
+
+[branch "main"]
+    remote = origin
+    merge = refs/heads/main
